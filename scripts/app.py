9dac043c1e6f8c1ed7c91d21315d164dbf7d5f76
Brett Kromkamp <brettkromkamp@gmail.com>
2022-02-05 11:03:07 +0100

    Fix tree traversal in 'app.py'

--- scripts/app.py
+++ scripts/app.py
@@ -13,15 +13,9 @@ tree = Tree()
 # A node without a parent pointer is by definition the root node
 tree.add_node("Elon Musk", node_type="person")
 
-tree.add_node(
-    "Lyndon Rive", parent_pointer="Elon Musk", node_type="person", edge_type="family"
-)
-tree.add_node(
-    "SpaceX", parent_pointer="Elon Musk", node_type="company", edge_type="founder"
-)
-tree.add_node(
-    "Tesla", parent_pointer="Elon Musk", node_type="company", edge_type="founder"
-)
+tree.add_node("Lyndon Rive", parent_pointer="Elon Musk", node_type="person", edge_type="family")
+tree.add_node("SpaceX", parent_pointer="Elon Musk", node_type="company", edge_type="founder")
+tree.add_node("Tesla", parent_pointer="Elon Musk", node_type="company", edge_type="founder")
 tree.add_node(
     "Solar City",
     parent_pointer="Lyndon Rive",
@@ -34,15 +28,9 @@ tree.add_node(
     node_type="product",
     edge_type="service",
 )
-tree.add_node(
-    "Falcon 9", parent_pointer="SpaceX", node_type="rocket", edge_type="technology"
-)
-tree.add_node(
-    "Falcon Heavy", parent_pointer="SpaceX", node_type="rocket", edge_type="technology"
-)
-tree.add_node(
-    "Dragon", parent_pointer="SpaceX", node_type="space-ship", edge_type="technology"
-)
+tree.add_node("Falcon 9", parent_pointer="SpaceX", node_type="rocket", edge_type="technology")
+tree.add_node("Falcon Heavy", parent_pointer="SpaceX", node_type="rocket", edge_type="technology")
+tree.add_node("Dragon", parent_pointer="SpaceX", node_type="space-ship", edge_type="technology")
 tree.add_node("Model S", parent_pointer="Tesla", node_type="car", edge_type="product")
 tree.add_node("Model X", parent_pointer="Tesla", node_type="car", edge_type="product")
 tree.add_node("Model Y", parent_pointer="Tesla", node_type="car", edge_type="product")
@@ -52,11 +40,9 @@ print("\n***** TREE STRUCTURE *****")
 tree.display("Elon Musk")
 
 print("\n***** DEPTH-FIRST ITERATION *****")
-for identifier in tree.traverse("Elon Musk"):
-    node = tree[identifier]
+for node in tree.traverse("Elon Musk"):
     print(f"{node.identifier} [{node.type or '*Undefined*'}]")
 
 print("\n***** BREADTH-FIRST ITERATION *****")
-for identifier in tree.traverse("Elon Musk", mode=TraversalMode.BREADTH):
-    node = tree[identifier]
+for node in tree.traverse("Elon Musk", mode=TraversalMode.BREADTH):
     print(f"{node.identifier} [{node.type or '*Undefined*'}]")
