b4465f1cd99a7716a99ac2361624ff03090f7897
Brett Kromkamp <brettkromkamp@gmail.com>
2022-02-10 19:03:05 +0100

    Minor updates in preparation for publishing

--- setup.py
+++ setup.py
@@ -32,12 +32,12 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=REQUIRED,
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
